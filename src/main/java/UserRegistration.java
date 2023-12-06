b34b2f5b3fb3e6604cd67782430ec41a1c30a510
Darkdriller <swaindhruv28@gmail.com>
2023-12-07 03:02:52 +0530

    Refactored: Added Exceptions

--- src/main/java/UserRegistration.java
+++ src/main/java/UserRegistration.java
@@ -1,23 +1,45 @@
+
 public class UserRegistration {
-    public boolean isValidFirstName(String firstName) {
-        return firstName.matches("[A-Z][a-z]{2,}");
+
+    public class UserRegistrationException extends Exception {
+        public UserRegistrationException(String message) {
+            super(message);
+        }
+    }
+    public boolean isValidFirstName(String firstName) throws UserRegistrationException {
+        if (!firstName.matches("[A-Z][a-z]{2,}")){
+            throw new UserRegistrationException("Invalid First Name");
+        }
+        else return  true;
     }
 
-    public boolean isValidLastName(String lastName) {
-        return lastName.matches("[A-Z][a-z]{2,}");
+    public boolean isValidLastName(String lastName) throws UserRegistrationException {
+        if (!lastName.matches("[A-Z][a-z]{2,}")){
+            throw new UserRegistrationException("Invalid Last Name");
+        }
+        else{
+            return true;
+        }
     }
 
-    public boolean isValidEmail(String email) {
-        return email.matches("[a-zA-Z0-9]+([.][a-zA-Z0-9]+)*([_+%-][a-zA-Z0-9]+)*@[a-zA-Z0-9-]+\\.[a-zA-Z]{2,}(\\.[a-zA-Z]{2,})?"
-
-        );
+    public boolean isValidEmail(String email) throws UserRegistrationException {
+        if (!email.matches("[a-zA-Z0-9]+([.][a-zA-Z0-9]+)*([_+%-][a-zA-Z0-9]+)*@[a-zA-Z0-9-]+\\.[a-zA-Z]{2,}(\\.[a-zA-Z]{2,})?")){
+            throw new UserRegistrationException("Invalid Email");
+        }
+        else return true;
     }
 
-    public boolean isValidMobile(String mobile) {
-        return mobile.matches("\\d{2} \\d{10}");
+    public boolean isValidMobile(String mobile) throws UserRegistrationException {
+        if (!mobile.matches("\\d{2} \\d{10}")){
+            throw new UserRegistrationException("Invalid Mobile");
+        }
+        else return true;
     }
 
-    public boolean isValidPassword(String password) {
-        return password.matches("(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+$).{8,}");
+    public boolean isValidPassword(String password) throws UserRegistrationException {
+        if (!password.matches("(?=.*[0-9])(?=.*[A-Z])(?=.*[@#$%^&+=]).{8,}")){
+            throw new UserRegistrationException("Invalid Password");
+        }
+        else return true;
     }
 }
--- src/test/java/UserRegistrationTest.java
+++ src/test/java/UserRegistrationTest.java
@@ -5,78 +5,105 @@ public class UserRegistrationTest {
 
     // Test cases for First Name
     @Test
-    public void testValidFirstName() {
+    public void testValidFirstName() throws UserRegistration.UserRegistrationException {
         Assert.assertTrue(userReg.isValidFirstName("Alice"));
     }
 
     @Test
     public void testInvalidFirstName() {
-        Assert.assertFalse(userReg.isValidFirstName("al"));
+        try {
+            Assert.assertFalse(userReg.isValidFirstName("al"));
+        }
+        catch (UserRegistration.UserRegistrationException e){
+            Assert.assertEquals(e.getMessage(), "Invalid First Name");
+        }
+
     }
 
     // Test cases for Last Name
     @Test
-    public void testValidLastName() {
+    public void testValidLastName() throws UserRegistration.UserRegistrationException {
         Assert.assertTrue(userReg.isValidLastName("Smith"));
     }
 
     @Test
     public void testInvalidLastName() {
-        Assert.assertFalse(userReg.isValidLastName("Sm"));
+        try {
+            Assert.assertFalse(userReg.isValidLastName("Sm"));
+        }
+        catch (UserRegistration.UserRegistrationException e){
+            Assert.assertEquals(e.getMessage(),"Invalid Last Name");
+        }
     }
 
     // Test cases for Email
     @Test
-    public void testValidEmail() {
-        Assert.assertTrue(userReg.isValidEmail("abc@yahoo.com"));
-        Assert.assertTrue(userReg.isValidEmail("abc-100@yahoo.com"));
-        Assert.assertTrue(userReg.isValidEmail("abc.100@yahoo.com"));
-        Assert.assertTrue(userReg.isValidEmail("abc111@abc.com"));
-        Assert.assertTrue(userReg.isValidEmail("abc-100@abc.net"));
-        Assert.assertTrue(userReg.isValidEmail("abc.100@abc.com.au"));
-        Assert.assertTrue(userReg.isValidEmail("abc@gmail.com.com"));
-        Assert.assertTrue(userReg.isValidEmail("abc+100@gmail.com"));
+    public void testValidEmail() throws UserRegistration.UserRegistrationException {
+            Assert.assertTrue(userReg.isValidEmail("abc@yahoo.com"));
+            Assert.assertTrue(userReg.isValidEmail("abc-100@yahoo.com"));
+            Assert.assertTrue(userReg.isValidEmail("abc.100@yahoo.com"));
+            Assert.assertTrue(userReg.isValidEmail("abc111@abc.com"));
+            Assert.assertTrue(userReg.isValidEmail("abc-100@abc.net"));
+            Assert.assertTrue(userReg.isValidEmail("abc.100@abc.com.au"));
+            Assert.assertTrue(userReg.isValidEmail("abc@gmail.com.com"));
+            Assert.assertTrue(userReg.isValidEmail("abc+100@gmail.com"));
+
     }
 
     @Test
-    public void testInvalidEmail() {
-        Assert.assertFalse(userReg.isValidEmail("abc"));
-        Assert.assertFalse(userReg.isValidEmail("abc@.com.my"));
-        Assert.assertFalse(userReg.isValidEmail("abc123@gmail.a"));
-        Assert.assertFalse(userReg.isValidEmail("abc123@.com"));
-        Assert.assertFalse(userReg.isValidEmail("abc123@.com.com"));
-        Assert.assertFalse(userReg.isValidEmail(".abc@abc.com"));
-        Assert.assertFalse(userReg.isValidEmail("abc()*@gmail.com"));
-        Assert.assertFalse(userReg.isValidEmail("abc@%*.com"));
-        Assert.assertFalse(userReg.isValidEmail("abc..2002@gmail.com"));
-        Assert.assertFalse(userReg.isValidEmail("abc.@gmail.com"));
-        Assert.assertFalse(userReg.isValidEmail("abc@abc@gmail.com"));
-        Assert.assertFalse(userReg.isValidEmail("abc@gmail.com.1a"));
-        Assert.assertFalse(userReg.isValidEmail("abc@gmail.com.aa.au"));
+    public void testInvalidEmail() throws UserRegistration.UserRegistrationException {
+        try {
+            userReg.isValidEmail("abc");
+            userReg.isValidEmail("abc@.com.my");
+            userReg.isValidEmail("abc123@gmail.a");
+            userReg.isValidEmail("abc123@.com");
+            userReg.isValidEmail("abc123@.com.com");
+            userReg.isValidEmail(".abc@abc.com");
+            userReg.isValidEmail("abc()*@gmail.com");
+            userReg.isValidEmail("abc@%*.com");
+            userReg.isValidEmail("abc..2002@gmail.com");
+            userReg.isValidEmail("abc.@gmail.com");
+            userReg.isValidEmail("abc@abc@gmail.com");
+            userReg.isValidEmail("abc@gmail.com.1a");
+            userReg.isValidEmail("abc@gmail.com.aa.au");
+        }
+        catch (UserRegistration.UserRegistrationException e){
+            Assert.assertEquals(e.getMessage(), "Invalid Email");
+        }
 
 
     }
 
     // Test cases for Mobile
     @Test
-    public void testValidMobile() {
+    public void testValidMobile() throws UserRegistration.UserRegistrationException {
         Assert.assertTrue(userReg.isValidMobile("91 1234567890"));
     }
 
     @Test
     public void testInvalidMobile() {
-        Assert.assertFalse(userReg.isValidMobile("911234567890"));
+        try {
+            userReg.isValidMobile("911234567890");
+        }
+        catch (UserRegistration.UserRegistrationException e){
+            Assert.assertEquals(e.getMessage(), "Invalid Mobile");
+        }
     }
 
     // Test cases for Password
     @Test
-    public void testValidPassword() {
+    public void testValidPassword() throws UserRegistration.UserRegistrationException {
         Assert.assertTrue(userReg.isValidPassword("Password1@"));
     }
 
     @Test
     public void testInvalidPassword() {
-        Assert.assertFalse(userReg.isValidPassword("pass"));
+        try {
+            Assert.assertFalse(userReg.isValidPassword("pass"));
+        }
+        catch (UserRegistration.UserRegistrationException e){
+            Assert.assertEquals(e.getMessage(), "Invalid Password");
+        }
     }
 }
 
